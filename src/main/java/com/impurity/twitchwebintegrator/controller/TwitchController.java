6d65f8a6bf1fc0b468a4c23f10146db9fb27f1bf
Tyler Kokoszka <techkoko77@gmail.com>
2019-01-18 17:06:34 -0500

    Added index controller

---
+++ src/main/java/com/impurity/twitchwebintegrator/controller/TwitchController.java
@@ -0,0 +1,16 @@
+package com.impurity.twitchwebintegrator.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class TwitchController {
+
+    @RequestMapping("/")
+    @ResponseBody
+    public String index() {
+        return "Hello, World!";
+    }
+
+}
