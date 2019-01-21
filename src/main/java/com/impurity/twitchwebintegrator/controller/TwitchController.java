0ce1018338da83642860991d8ac4cfe2fdfb5892
Tyler Kokoszka <techkoko77@gmail.com>
2019-01-21 10:20:36 -0500

    Changed cors

--- src/main/java/com/impurity/twitchwebintegrator/controller/TwitchController.java
+++ src/main/java/com/impurity/twitchwebintegrator/controller/TwitchController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin(origins = "https://tmk2003.github.io/Im-Generic-Username", maxAge = 3600)
+@CrossOrigin(origins = "https://tmk2003.github.io", maxAge = 3600)
 @RestController
 public class TwitchController {
 
