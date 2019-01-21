ebb670edf21196da6d8d61bf61a2e5388b74aade
Tyler Kokoszka <techkoko77@gmail.com>
2019-01-21 15:55:43 -0500

    Added config folder and added banner for spring

--- src/main/java/com/impurity/twitchwebintegrator/TwitchWebIntegratorApplication.java
+++ src/main/java/com/impurity/twitchwebintegrator/TwitchWebIntegratorApplication.java
@@ -3,9 +3,11 @@ package com.impurity.twitchwebintegrator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Entry of the application
+ */
 @SpringBootApplication
 public class TwitchWebIntegratorApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(TwitchWebIntegratorApplication.class, args);
 	}
