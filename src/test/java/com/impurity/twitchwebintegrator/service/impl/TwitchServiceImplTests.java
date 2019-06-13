29ef304a9dbabafd5b17e661aadf9007d074e143
Tyler Kokoszka <techkoko77@gmail.com>
2019-06-12 23:16:22 -0400

    Added jacoco

--- src/test/java/com/impurity/twitchwebintegrator/service/impl/TwitchServiceImplTests.java
+++ src/test/java/com/impurity/twitchwebintegrator/service/impl/TwitchServiceImplTests.java
@@ -1,6 +1,10 @@
 package com.impurity.twitchwebintegrator.service.impl;
 
+import com.impurity.twitchwebintegrator.model.SteamGame;
+import com.impurity.twitchwebintegrator.test.utils.SteamFactory;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -8,6 +12,10 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import static com.impurity.twitchwebintegrator.constant.Profiles.UNIT_TEST;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 /**
  * @author tmk2003
@@ -17,4 +25,17 @@ import static com.impurity.twitchwebintegrator.constant.Profiles.UNIT_TEST;
 @ActiveProfiles(UNIT_TEST)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class TwitchServiceImplTests {
+
+    @Test
+    @DisplayName("When getting a user, and ")
+    public void steam_library_return_200() throws Exception {
+//        SteamGame[] steamGames = SteamFactory.getValidSteamGameArray(10);
+//
+//        when(_mockSteamService.getGameLibrary(_mockChannelName)).thenReturn(steamGames);
+//        _mockMvc.perform(get("/steam/library/" + _mockChannelName))
+//                .andExpect(status().isOk())
+//                .andExpect(content().json(
+//                        mapToJson(steamGames)
+//                ));
+    }
 }
