17a9b60812f3a1cf8254b73dbb1d206c43c7a47e
Tyler Kokoszka <techkoko77@gmail.com>
2019-06-17 23:42:59 -0400

    Added exception tests

---
+++ src/test/java/com/impurity/twitchwebintegrator/exception/twitch/TwitchUserCreationExceptionTests.java
@@ -0,0 +1,29 @@
+package com.impurity.twitchwebintegrator.exception.twitch;
+
+import com.impurity.twitchwebintegrator.test.utils.AbstractTest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static com.impurity.twitchwebintegrator.constant.Profiles.UNIT_TEST;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * @author tmk2003
+ */
+@ExtendWith(SpringExtension.class)
+@ActiveProfiles(UNIT_TEST)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class TwitchUserCreationExceptionTests extends AbstractTest {
+
+    @Test
+    @DisplayName("The twitch user creation exception stores cause properly")
+    public void captures_cause() {
+        Exception exception = new Exception();
+        assertEquals(new TwitchUserCreationException("apples", exception).getCause(), exception);
+    }
+}
+
