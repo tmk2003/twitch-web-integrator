5c112ed2d05da9d29c3e2d6ba0fb55aaecd40001
Tyler Kokoszka <techkoko77@gmail.com>
2019-06-17 22:32:07 -0400

    Reordered modifiers

--- src/main/java/com/impurity/twitchwebintegrator/constant/Profiles.java
+++ src/main/java/com/impurity/twitchwebintegrator/constant/Profiles.java
@@ -4,8 +4,8 @@ package com.impurity.twitchwebintegrator.constant;
  * @author tmk2003
  */
 public final class Profiles {
-    public final static String PRODUCTION = "production";
-    public final static String STAGING = "staging";
-    public final static String LOCAL = "local";
-    public final static String UNIT_TEST = "unit-test";
+    public static final String PRODUCTION = "production";
+    public static final String STAGING = "staging";
+    public static final String LOCAL = "local";
+    public static final String UNIT_TEST = "unit-test";
 }
