21639bfaa35c577dcb449739fc3d3d414bbbf885
Tyler Kokoszka <techkoko77@gmail.com>
2019-06-17 23:16:59 -0400

    Added private constructor to profiles

--- src/main/java/com/impurity/twitchwebintegrator/constant/Profiles.java
+++ src/main/java/com/impurity/twitchwebintegrator/constant/Profiles.java
@@ -4,6 +4,7 @@ package com.impurity.twitchwebintegrator.constant;
  * @author tmk2003
  */
 public final class Profiles {
+    private Profiles() {}
     public static final String PRODUCTION = "production";
     public static final String STAGING = "staging";
     public static final String LOCAL = "local";
