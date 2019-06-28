3d1735d76eca101dda1b5cfe95dd489dc3534eea
Tyler Kokoszka <techkoko77@gmail.com>
2019-06-27 21:20:14 -0400

    added exception tests for osrs

---
+++ src/test/java/com/impurity/twitchwebintegrator/osrs/exception/OsrsClientPlayerHttpRequestExceptionTests.java
@@ -0,0 +1,44 @@
+package com.impurity.twitchwebintegrator.osrs.exception;
+
+import com.impurity.twitchwebintegrator.test.utils.AbstractTest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static com.impurity.twitchwebintegrator.constant.Profiles.UNIT_TEST;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * @author tmk2003
+ */
+@ExtendWith(SpringExtension.class)
+@ActiveProfiles(UNIT_TEST)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class OsrsClientPlayerHttpRequestExceptionTests extends AbstractTest {
+
+    @Test
+    @DisplayName("The osrs player request exception stores message properly")
+    public void captures_message() {
+        String testMessage = "apples";
+        assertEquals(new OsrsClientItemHttpRequestException(testMessage, HttpStatus.SERVICE_UNAVAILABLE, new Exception()).getMessage(), testMessage);
+    }
+
+    @Test
+    @DisplayName("The osrs player request exception stores message properly")
+    public void captures_throwable() {
+        Exception testException = new Exception();
+        assertEquals(new OsrsClientItemHttpRequestException("apples", HttpStatus.SERVICE_UNAVAILABLE, testException).getCause(), testException);
+    }
+
+    @Test
+    @DisplayName("The osrs player request exception stores message properly")
+    public void captures_status() {
+        HttpStatus testStatus = HttpStatus.ACCEPTED;
+        assertEquals(new OsrsClientItemHttpRequestException("apples",testStatus, new Exception()).getStatus(), testStatus);
+    }
+}
+
